e1edb4a8f1e38c58c3062c1d1dbdad4f369ec451
Harshit Singh <92515730+harshit450@users.noreply.github.com>
2024-03-18 06:32:23 +0000

    Change

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId= 12345678
+let  accountEmail = "raj@gmail.com"
+var  accountPassword = "2187890"
+accountCity = "Jaipur"
+
+
+accountEmail = "raj@789"
+accountPassword = "6578956"
+accountCity = "Kolkata"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
